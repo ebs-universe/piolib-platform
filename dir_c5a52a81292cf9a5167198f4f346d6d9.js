82962e674815335385162341d5f2776c8f595293
chintal <chintal@users.noreply.github.com>
2024-05-29 15:18:17 +0000

    deploy: bcd9a71f743d7ac4ecd61bc7a456b68ebca7d1f9

--- dir_c5a52a81292cf9a5167198f4f346d6d9.js
+++ dir_c5a52a81292cf9a5167198f4f346d6d9.js
@@ -3,6 +3,8 @@ var dir_c5a52a81292cf9a5167198f4f346d6d9 =
     [ "asm.h", "asm_8h.html", null ],
     [ "config.h", "config_8h_source.html", null ],
     [ "cpu.h", "cpu_8h.html", null ],
+    [ "debug.c", "debug_8c_source.html", null ],
+    [ "debug.h", "debug_8h_source.html", null ],
     [ "romem.h", "romem_8h_source.html", null ],
     [ "transport.h", "transport_8h.html", "transport_8h" ],
     [ "types.h", "types_8h_source.html", null ]
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -6,11 +6,13 @@ var NAVTREEINDEX0 =
 "config_8h_source.html":[1,0,0,1],
 "cpu_8h.html":[1,0,0,2],
 "cpu_8h_source.html":[1,0,0,2],
+"debug_8c_source.html":[1,0,0,3],
+"debug_8h_source.html":[1,0,0,4],
 "dir_c5a52a81292cf9a5167198f4f346d6d9.html":[1,0,0],
 "files.html":[1,0],
 "index.html":[],
 "pages.html":[],
-"romem_8h_source.html":[1,0,0,3],
+"romem_8h_source.html":[1,0,0,5],
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html":[0,0,0],
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html#a1e75bccd0531a8b506c2398cd37b7e8f":[0,0,0,0],
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html#a2ddf9341ed41a034349f9b8731125913":[0,0,0,4],
@@ -19,8 +21,8 @@ var NAVTREEINDEX0 =
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html#a82e31eb1b77ac38684d071fabcd9a8f0":[0,0,0,5],
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html#ac242e1b8e75a3776a4efd709d21af3d0":[0,0,0,2],
 "struct_p_l_u_g_g_a_b_l_e___t_r_a_n_s_p_o_r_t__t.html#ace14fa1fa6cd2c050a8b71ec3c76e082":[0,0,0,1],
-"transport_8h.html":[1,0,0,4],
-"transport_8h.html#a49671e33cde8f7d9df622dba0108322b":[1,0,0,4,1],
-"transport_8h_source.html":[1,0,0,4],
-"types_8h_source.html":[1,0,0,5]
+"transport_8h.html":[1,0,0,6],
+"transport_8h.html#a49671e33cde8f7d9df622dba0108322b":[1,0,0,6,1],
+"transport_8h_source.html":[1,0,0,6],
+"types_8h_source.html":[1,0,0,7]
 };
