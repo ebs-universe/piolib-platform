e6d814998631c26e8a1632f9ddddc149b0e93d00
Chintalagiri Shashank <shashank@chintal.in>
2024-11-06 12:27:06 +0530

    Add error output

--- src/platform/debug.c
+++ src/platform/debug.c
@@ -2,8 +2,35 @@
 
 #include "cpu.h"
 #include "debug.h"
+#include "types.h"
+#include <application.h>
+
+#if APP_ENABLE_OUTPUT_ERROR
+#include <hal/uc/gpio.h>
+#endif
+
+#ifdef __arm__
+
+typedef union {
+    struct {
+        uint8_t MMFSR;
+        uint8_t BFSR;
+        uint16_t UFSR;
+    };
+    uint32_t CFSR;
+} CFSR_t;
+
+#endif
+
 
 void die(void) {
     global_interrupt_disable();
+    #ifdef __arm__
+    CFSR_t cfsr = (CFSR_t)(*((HAL_SFR_t *)0xE000ED28));
+    (void) cfsr;
+    #endif
+    #if APP_ENABLE_OUTPUT_ERROR
+        gpio_set_output_high(ERROR_GPIO);
+    #endif
     while(1);
 }
