0bc66e46a2a1499b43c260d78c76289e605f9056
Chintalagiri Shashank <shashank@chintal.in>
2024-11-08 19:02:09 +0530

    Make error uot polarity configurable

--- src/platform/debug.c
+++ src/platform/debug.c
@@ -30,7 +30,11 @@ void die(void) {
     (void) cfsr;
     #endif
     #if APP_ENABLE_OUTPUT_ERROR
+        #if ERROR_POLARITY
         gpio_set_output_high(ERROR_GPIO);
+        #else
+        gpio_set_output_low(ERROR_GPIO);
+        #endif
     #endif
     while(1);
 }
