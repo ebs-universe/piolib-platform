11fdb1efde66d399bcbf609a9087ba9f1a015f32
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2025-01-12 19:03:02 +0530

    Add debug interface

--- src/platform/debug.c
+++ src/platform/debug.c
@@ -37,7 +37,7 @@ void die(void) {
     #if APP_ENABLE_OUTPUT_ERROR
         #if ERROR_POLARITY
         gpio_set_output_high(ERROR_GPIO);
-        #else
+    #else           
         gpio_set_output_low(ERROR_GPIO);
         #endif
     #endif
@@ -45,3 +45,53 @@ void die(void) {
 }
 
 #pragma GCC pop_options 
+
+#if DEBUG_TRANSPORT_TYPE == EBS_INTF_UART
+#include <hal/uc/uart.h>
+
+#define LOCKTOKEN   0x00
+
+HAL_BASE_t _debug_putchar(char c){
+    if (uart_reqlock(DEBUG_TRANSPORT_INTFNUM, 1, LOCKTOKEN)){
+        return uart_putc(DEBUG_TRANSPORT_INTFNUM, c, LOCKTOKEN, 0x00);
+    }
+    uart_send_trigger(DEBUG_TRANSPORT_INTFNUM);
+    return 0;
+}
+
+HAL_BASE_t _debug_write(const void *buffer, HAL_BASE_t len){
+    if (uart_reqlock(DEBUG_TRANSPORT_INTFNUM, len, LOCKTOKEN)){
+        return uart_write(DEBUG_TRANSPORT_INTFNUM, (uint8_t *)buffer, len, LOCKTOKEN);
+    }
+    uart_send_trigger(DEBUG_TRANSPORT_INTFNUM);
+    return 0;
+}
+
+HAL_BASE_t _debug_printf(const char *format, va_list args){
+    return uart_vprintf(DEBUG_TRANSPORT_INTFNUM, format, args);
+}
+
+void _debug_flush(void){
+    uart_send_flush(DEBUG_TRANSPORT_INTFNUM);
+}
+
+#elif DEBUG_TRANSPORT_TYPE == EBS_INTF_USBCDC
+#include <hal/uc/usbcdc.h>
+
+HAL_BASE_t _debug_putchar(char c){
+
+}
+
+HAL_BASE_t _debug_write(const void *buffer, HAL_BASE_t len){
+
+}
+
+HAL_BASE_t _debug_printf(const char *format, ...){
+
+}
+
+void _debug_flush(void){
+
+}
+
+#endif
--- src/platform/debug.h
+++ src/platform/debug.h
@@ -1,3 +1,58 @@
 
 
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stdarg.h>
+#include "platform/types.h"
+
+
 void die(void);
+
+HAL_BASE_t _debug_putchar(char c);
+HAL_BASE_t _debug_write(const void *buffer, HAL_BASE_t len);
+HAL_BASE_t _debug_printf(const char *format, va_list args);
+void _debug_flush(void);
+
+#if APP_ENABLE_DEBUG
+
+static inline HAL_BASE_t debug_putchar(char c) {
+    return _debug_putchar(c);
+}
+
+static inline HAL_BASE_t debug_write(const void *buffer, HAL_BASE_t len){
+    return _debug_write(buffer, len);
+}
+
+static inline HAL_BASE_t debug_printf(const char *format, ...){
+    uint8_t rval;
+    va_list args;
+    va_start( args, format );
+    rval = _debug_printf(format, args);
+    va_end(args);
+    return rval;
+}
+
+static inline void debug_flush(void){
+    _debug_flush();
+}
+
+#else
+
+static inline HAL_BASE_t debug_putchar(char c) {
+    return 0;
+}
+
+static inline HAL_BASE_t debug_write(const void *buffer, HAL_BASE_t len){
+    return 0;
+}
+
+static inline HAL_BASE_t debug_printf(const char *format, ...){
+    return 0;
+}
+
+static inline void debug_flush(void){}
+
+#endif 
+
+#endif
